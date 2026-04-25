d7be8a328c5e784054a0b276f122b43ba9bd21c6
Nicolas <nilasworks@gmail.com>
2026-04-24 22:22:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+    const ScreenMode = document.getElementById("DarkMode");
+    const buttonSobre = document.getElementById("Sobre");
+    const aboutWindow = document.getElementById("about-window");
+    const body = document.body;
+    const inputTarefa = document.getElementById("inputTarefa");
+    const buttonAdd = document.getElementById("btnAdicionar");
+    const buttonRev =  document.createElement("button");
+    let mode = false;
+    function button(){
+      if(inputTarefa.value.length >= 5 ){
+         const li = document.createElement("li");
+        const p = document.createElement("p");
+        const buttonRev = document.createElement("button");
+
+        p.innerText = inputTarefa.value;
+        buttonRev.innerText = "remover";
+
+        
+        buttonRev.onclick = function() {
+            li.remove();
+        };
+
+        li.appendChild(p);
+        li.appendChild(buttonRev);
+        list.appendChild(li);
+
+        inputTarefa.value = "";
+     }else{
+        alert("Escreva uma tarefa maior que 5 palavras");
+     }
+    }
+    function buttonScreen(){
+        if(mode != true  ){
+          ScreenMode.innerText = "LigthMode";
+          mode = true;
+        }else{
+          ScreenMode.innerText = "DarkMode";
+          mode = false;
+        }
+    }
+    function buttonScreen(){
+        if(!mode){
+            ScreenMode.innerText = "LightMode";
+            document.body.classList.add("dark");
+            mode = true;
+        }else{
+            ScreenMode.innerText = "DarkMode";
+            document.body.classList.remove("dark");
+            mode = false;
+        }
+    }
